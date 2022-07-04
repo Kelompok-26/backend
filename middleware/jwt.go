999a250db73c0be7bec04f657f903c7e17fdc186
Galang Prang <galangprang1@gmail.com>
2022-07-04 19:30:58 +0800

    apply jwt

--- middleware/jwt.go
+++ middleware/jwt.go
@@ -2,17 +2,64 @@ package middleware
 
 import (
 	"backend/constants"
+	"net/http"
+	"strings"
 	"time"
 
 	"github.com/golang-jwt/jwt"
+	"github.com/labstack/echo/v4"
 )
 
-func CreateToken(adminID int, PhoneNumber int) (string, error) {
+func CreateToken(id int, PhoneNumber string, role string) (string, error) {
 	claims := jwt.MapClaims{}
-	claims["adminID"] = adminID
+	claims["id"] = id
 	claims["PhoneNumber"] = PhoneNumber
 	claims["exp"] = time.Now().Add(time.Hour * 24).Unix()
 
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 	return token.SignedString([]byte(constants.SECRET_JWT))
 }
+
+func GetToken(c echo.Context) string {
+	header := c.Request().Header.Get("Authorization")
+	token := strings.Split(header, " ")[1]
+	return token
+}
+
+type Claims struct {
+	jwt.StandardClaims
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+	Role string `json:"role"`
+	Exp  int64  `json:"exp"`
+}
+
+func ParseJWT(tokenstr string) (*Claims, error) {
+	hmacSecret := constants.SECRET_JWT
+	var claims *Claims
+
+	token, err := jwt.ParseWithClaims(tokenstr, &Claims{}, func(t *jwt.Token) (interface{}, error) {
+		return []byte(hmacSecret), nil
+	})
+
+	if token != nil {
+		if claims, ok := token.Claims.(*Claims); ok && token.Valid {
+			return claims, nil
+		}
+	}
+
+	return claims, err
+}
+
+func AdminRole(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		token := GetToken(c)
+		claims, _ := ParseJWT(token)
+		if claims.Role != "admin" {
+			return c.JSON(http.StatusForbidden, "admin")
+		}
+
+		return next(c)
+	}
+}
+
--- routers/routes.go
+++ routers/routes.go
@@ -1,25 +1,32 @@
 package routers
 
 import (
+	"backend/constants"
 	"backend/controllers"
 
+	// "backend/middleware"
+
 	"github.com/labstack/echo/v4"
+	eMiddleware "github.com/labstack/echo/v4/middleware"
 )
 
 func Router() *echo.Echo {
 	e := echo.New()
 
+	//login
+	e.POST("/user/login", controllers.LoginUser)
+	e.POST("/admin/login", controllers.LoginAdminController)
+
 	// Users
-	e.POST("/login", controllers.LoginUser)
 	e.POST("/users", controllers.CreateUserControllers)
-	e.GET("/users", controllers.GetAllusercontrollers)
+	e.GET("/users", controllers.GetAllusercontrollers, eMiddleware.JWT([]byte(constants.SECRET_JWT)))
 	e.GET("/users/:uid", controllers.GetUserControllers)
 	e.PUT("/users/:uid", controllers.UpdateUserControllers)
 	e.DELETE("/users/:uid", controllers.DeleteUserControllers)
 
 	// Products
 	e.POST("/products", controllers.CreateProductControllers)
-	e.GET("/products", controllers.GetAllProductControllers)
+	e.GET("/products", controllers.GetAllProductControllers, eMiddleware.JWT([]byte(constants.SECRET_JWT)))
 	e.GET("/products/:pid", controllers.GetProductControllers)
 	e.PUT("/products/:pid", controllers.UpdateProductControllers)
 	e.DELETE("/products/:pid", controllers.DeleteProductControllers)
@@ -30,15 +37,20 @@ func Router() *echo.Echo {
 	e.GET("/products/Emoney", controllers.GetEmoney)
 	e.GET("/products/Cashout", controllers.GetCashout)
 
+	//transaction
 	e.GET("/redeem", controllers.GetAllRedeemControllers)
 	e.GET("/user/redeem/:user_id", controllers.GetRedeemByUserIDControllers)
 	e.POST("/redeem", controllers.CreateRedeemsControllers)
 	e.PUT("/redeem/:id", controllers.UpdateRedeemControllers)
 	e.DELETE("/redeem/:id", controllers.DeleteRedeemControllers)
 
+	//transaction
 	e.GET("/transaction", controllers.GetAllTransactionControllers)
-	e.GET("/transaction/:id", controllers.GetTransactionIDControllers)
+	e.GET("/transaction/user/:id", controllers.GetTransactionByIdUserControllers)
+	e.GET("/transaction/:id", controllers.GetTransactionByIdControllers)
 	e.POST("/transaction", controllers.CreateTransactionsControllers)
+	e.PUT("/transaction", controllers.UpdatetransactionControllers)
+	e.DELETE("/transaction", controllers.DeleteTansactionControllers)
 
 	return e
 }
