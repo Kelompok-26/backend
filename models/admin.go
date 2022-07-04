1b4aba0ab4921fc7d7d0710236725f8ce6f9b739
Galang Prang <galangprang1@gmail.com>
2022-07-04 19:43:59 +0800

    fix phone_number

--- models/admin.go
+++ models/admin.go
@@ -2,6 +2,6 @@ package models
 
 type Admin struct {
 	ID          int    `json:"id" form:"id"`
-	PhoneNumber int    `gorm:"column:phonenumber" json:"phonenumber"`
+	PhoneNumber int    `gorm:"column:phone_number" json:"phone_number"`
 	Password    string `json:"password" form:"password"`
 }
