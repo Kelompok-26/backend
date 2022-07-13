ccb9c8bba9377016179ec761e25b7d821019b6f9
Galang Prang <galangprang1@gmail.com>
2022-07-13 22:31:52 +0800

    add func create trans, fixing date response

--- controllers/transaction.go
+++ controllers/transaction.go
@@ -2,8 +2,11 @@ package controllers
 
 import (
 	"backend/config"
+	"backend/controllers/request"
+	"backend/controllers/response"
 	"backend/helper"
 	"backend/models"
+	"fmt"
 	"net/http"
 	"strconv"
 
@@ -16,7 +19,7 @@ func GetAllTransactionControllers(c echo.Context) error {
 		return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
 	}
 
-	return c.JSON(http.StatusOK, helper.BuildResponse("success get all transaction", transaction))
+	return c.JSON(http.StatusOK, helper.BuildResponse("success get all transaction", response.MapToBatchTransaction(transaction)))
 }
 func GetTransactionByIdControllers(c echo.Context) error {
 	id, _ := strconv.Atoi(c.Param("id"))
@@ -31,7 +34,7 @@ func GetTransactionByIdControllers(c echo.Context) error {
 		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
 	}
 
-	return c.JSON(http.StatusOK, helper.BuildResponse("success get transaction", transaction))
+	return c.JSON(http.StatusOK, helper.BuildResponse("success get transaction", response.MapToBatchTransaction(transaction)))
 }
 
 func GetTransactionByIdUserControllers(c echo.Context) error {
@@ -47,24 +50,13 @@ func GetTransactionByIdUserControllers(c echo.Context) error {
 		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
 	}
 
-	return c.JSON(http.StatusOK, helper.BuildResponse("success get transaction", transaction))
-}
-
-func CreateTransactionsControllers(c echo.Context) error {
-	transaction := models.InsertTransaction{}
-	c.Bind(&transaction)
-
-	if err := config.DB.Table("transaction").Create(&transaction).Error; err != nil {
-		return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
-	}
-
-	return c.JSON(http.StatusCreated, helper.BuildResponse("success create new transaction", transaction))
+	return c.JSON(http.StatusOK, helper.BuildResponse("success get transaction", response.MapToBatchTransaction(transaction)))
 }
 
 // delete transaction by id
 func DeleteTansactionControllers(c echo.Context) error {
 	id := c.Param("id")
-	transaction := models.InsertTransaction{}
+	transaction := models.Transaction{}
 
 	if err := config.DB.Table("transaction").Where("id", id).Find(&transaction).Error; err != nil {
 		if err.Error() == "record not found" {
@@ -80,13 +72,13 @@ func DeleteTansactionControllers(c echo.Context) error {
 		return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
 	}
 
-	return c.JSON(http.StatusOK, helper.BuildResponse("transaction deleted successfully", transaction))
+	return c.JSON(http.StatusOK, helper.BuildResponse("transaction deleted successfully", transaction.Id))
 }
 
 // update by id
 func UpdatetransactionControllers(c echo.Context) error {
 	id := c.Param("id")
-	transaction := models.InsertTransaction{}
+	transaction := models.Transaction{}
 
 	if err := config.DB.Table("transaction").Where("id", id).Find(&transaction).Error; err != nil {
 		if err.Error() == "record not found" {
@@ -98,16 +90,72 @@ func UpdatetransactionControllers(c echo.Context) error {
 		return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
 	}
 
-	newtransaction := models.InsertTransaction{}
+	newtransaction := models.Transaction{}
 	c.Bind(&newtransaction)
 
 	transaction.User = newtransaction.User
 	transaction.Product = newtransaction.Product
-	transaction.Point = newtransaction.Point
-	transaction.Total = newtransaction.Total
+	// transaction.Point = newtransaction.Point
+	// transaction.Total = newtransaction.Total
 	if err := config.DB.Table("transaction").Where("id", id).Save(&transaction).Error; err != nil {
 		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
 	}
 
-	return c.JSON(http.StatusOK, helper.BuildResponse("success update transaction", transaction))
+	return c.JSON(http.StatusOK, helper.BuildResponse("success update transaction", response.MapToTransaction(transaction)))
+}
+
+func UserCreateTransactionsController(c echo.Context) error {
+	id, _ := strconv.Atoi(c.Param("id"))
+	reqtrans := request.ReqTransaction{}
+
+	if err := c.Bind(&reqtrans); err != nil {
+		fmt.Println(err)
+		return c.String(http.StatusInternalServerError, "Internal Server Error ")
+	}
+	transactions := reqtrans.MapToTransaction()
+	transactions.UserId = id
+	// Get User By ID
+	var user models.User
+	if err := config.DB.Table("users").First(&user, id).Error; err != nil {
+		fmt.Println(err)
+		return c.String(http.StatusNotFound, "User not found")
+	}
+	// Get product By ID
+	var product models.Product
+	if err := config.DB.Table("products").First(&product, transactions.ProductId).Error; err != nil {
+		fmt.Println(err)
+		return c.String(http.StatusNotFound, "product not found")
+	}
+	// Point User < product.Point
+	if user.Point < product.Point {
+		fmt.Println(user.Point)
+		return c.String(http.StatusBadRequest, "Not Enough Point")
+	}
+
+	// Stock = 0
+	if product.Stock == 0 {
+		return c.String(http.StatusBadRequest, "Stock Out")
+	} else {
+		// Reduce product Stock By 1
+		if err := config.DB.Table("products").Model(&product).Update("stock", product.Stock-1).Error; err != nil {
+			fmt.Println(err)
+			return c.String(http.StatusInternalServerError, "Internal Server Error ")
+		}
+	}
+	// Reduce Point User
+	if err := config.DB.Table("users").Model(&user).Where("id = ?", user.Id).Update("point", user.Point-product.Point).Error; err != nil {
+		fmt.Println(err)
+		return c.String(http.StatusInternalServerError, "Internal Server Error")
+	}
+	if err := config.DB.Table("transaction").Create(&transactions).Error; err != nil {
+		fmt.Println(err)
+		return c.String(http.StatusInternalServerError, "Internal Server Error")
+	}
+
+	if err := config.DB.Table("transaction").Preload("User").Preload("Product").Find(&transactions).Error; err != nil {
+		fmt.Println(err)
+		return c.String(http.StatusInternalServerError, "Internal Server Error")
+	}
+
+	return c.JSON(http.StatusOK, response.MapToTransaction(transactions))
 }
